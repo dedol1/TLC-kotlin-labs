0179070d374890e8a5a3bc96acaeb61cd9b168d5
dedol1 <afoteylaryeasamuel@gmail.com>
2021-10-13 16:58:58 +0000

    updates

--- labTwo/src/main/kotlin/Main.kt
+++ labTwo/src/main/kotlin/Main.kt
@@ -1,3 +1,27 @@
+// creating a function called balance
+
+fun balance() : Unit{
+    println("your balance is $funds")
+}
+
+// function for deposit
+fun deposit(depositMoney: Double) : Unit{
+        funds += depositMoney
+}
+
+// function for withdrawal
+
+fun withdraw (withDrawMoney : Double) : Unit{
+    print("Please enter your password to authenticate this process: ")
+    val password : String = readLine()!!
+
+    if (password == pswd){
+        funds -= withDrawMoney
+    }
+    else println("wrong password, restart your withdraw process")
+}
+
+
 var funds : Double = 100.0
 val pswd = "password"
 
@@ -9,8 +33,11 @@ fun main() {
         print("Command: ")
         input = readLine()!!
         cmd = input.split(" ")
-        when (cmd[0]) {
+        when (cmd[0].lowercase()) {
             // Each command goes here...
+            "balance" -> balance()
+            "deposit" -> deposit(depositMoney = cmd[1].toDouble())
+            "withdraw" -> withdraw(withDrawMoney = cmd[1].toDouble())
             else -> println("Invalid command")
         }
     }
